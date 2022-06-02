b148e48c8bac90f7db137ce8b7cf92dbe541feb7
Breno A Froes P <breno.froes@timeprimo.com>
2022-06-02 18:50:55 -0300

    Initial commit

---
+++ frontend/consultas-conexa-saude/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Home</h1>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  mounted () {
+    this.$http.get('http://localhost:3333/consultations?_expand=patient').then(res => {
+      console.log(res)
+    })
+  }
+}
+</script>
