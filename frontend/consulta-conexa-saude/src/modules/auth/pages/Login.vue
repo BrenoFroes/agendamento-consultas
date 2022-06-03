ca79474077f6a06f0949bc5bee8552d0621e1c96
Breno A Froes P <breno.froes@timeprimo.com>
2022-06-02 21:49:36 -0300

    [feature] Doing authentication

--- frontend/consulta-conexa-saude/src/modules/auth/pages/Login.vue
+++ frontend/consulta-conexa-saude/src/modules/auth/pages/Login.vue
@@ -1,5 +1,20 @@
 <template>
-  <h1>Login</h1>
+  <div class="container">
+    <form @submit.prevent="submit()">
+      <div class="card my-20">
+        <div class="card-header">
+          <h1 class="text-xl text-red-600 font-bold uppercase">Login</h1>
+        </div>
+        <div class="card-body my-10">
+          <div class="form-group">
+            <input required v-model="form.email" placeholder="E-mail" type="email" class="form-control">
+            <input required v-model="form.password" placeholder="Password" type="password" class="form-control">
+          </div>
+        </div>
+        <button class="btn btn-primary">Send</button>
+      </div>
+    </form>
+  </div>
 </template>
 <script>
 import { mapActions } from 'vuex'
@@ -10,13 +25,15 @@ export default {
       password: ''
     }
   }),
-  created () {
-    this.ActionSetUser({ name: 'Fulano', email: 'email@email.com' })
-  },
   methods: {
-    ...mapActions('auth', ['ActionSetUser']),
-    submit () {
-      console.log(this.form)
+    ...mapActions('auth', ['ActionDoLogin']),
+    async submit () {
+      try {
+        await this.ActionDoLogin(this.form)
+        this.$router.push({ name: 'home' })
+      } catch (err) {
+        alert(err.body ? err.body.message : 'Não foi possível fazer o login')
+      }
     }
   }
 }
--- frontend/consulta-conexa-saude/src/modules/auth/store/actions.js
+++ frontend/consulta-conexa-saude/src/modules/auth/store/actions.js
@@ -1,5 +1,17 @@
 import * as types from './mutation-types'
+import services from '@/http'
+
+export const ActionDoLogin = ({ dispatch }, payload) => {
+  return services.auth.login(payload).then(res => {
+    dispatch('ActionSetUser', res.body.name)
+    dispatch('ActionSetToken', res.body.token)
+  })
+}
 
 export const ActionSetUser = ({ commit }, payload) => {
   commit(types.SET_USER, payload)
+}
+
+export const ActionSetToken = ({ commit }, payload) => {
+  commit(types.SET_TOKEN, payload)
 }
